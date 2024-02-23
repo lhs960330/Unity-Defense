00f7482840ed6eef586d0a31271e8eacf87760c8
Lee Hyoseok <lhs19960330@gmail.com>
2024-02-23 11:44:42 +0900

    feat : 점심시간

---
+++ Assets/Scripts/UIs/BaseUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseUI : MonoBehaviour
+{
+    protected Dictionary<string, RectTransform> transforms;
+    protected Dictionary<string, Button> buttons;
+    protected Dictionary<string, TMP_Text> texts;
+
+    
+    protected virtual void Awake()
+    {
+        Bind();
+    }
+
+    private void Bind()
+    {
+        transforms = new Dictionary<string, RectTransform>();
+        buttons = new Dictionary<string, Button>();
+        texts = new Dictionary<string, TMP_Text>();
+
+        RectTransform[] children = GetComponentsInChildren<RectTransform>();
+        foreach(RectTransform child in children)
+        {
+            string name = child.gameObject.name;
+
+            if (transforms.ContainsKey(name)) continue;
+
+            transforms.Add(name, child);
+
+            Button button = child.GetComponent<Button>();
+            if(button != null )
+            {
+                buttons.Add(name, button);
+            }
+            TMP_Text text = child.GetComponent<TMP_Text>();
+            if(text != null)
+            {
+                texts.Add(name, text);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UIs/TestUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestUI : BaseUI
+{
+    protected override void Awake()
+    {
+        base.Awake(); // 바인딩 과정 진행에 필요
+
+        texts["Tltle"].text = "Title";
+        buttons["NextButton"].onClick.AddListener(Jump) ;
+    }
+    public void Jump()
+    {
+        Debug.Log("Jump");
+    }
+}
